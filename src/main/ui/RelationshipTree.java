566b3d67dcfbbd2a9a0f74d6602c5b22b8128714
Emilia <enonar@students.cs.ubc.ca>
2025-10-20 11:33:12 -0700

    added specification of RelationshipTree class

---
+++ src/main/ui/RelationshipTree.java
@@ -0,0 +1,16 @@
+package ui;
+
+import model.CharacterGroup;
+
+public class RelationshipTree {
+    private CharacterGroup family;
+
+    // The relationship tree of the given character
+    public RelationshipTree(Character chara) {
+
+    }
+
+    // EFFECTS: draws the relationshiop tree of the given character
+    public void drawTree() {}
+    
+}
